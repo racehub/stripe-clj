30170a720fe6af51b95d82dd3246582e13d57988
Sam Ritchie <sritchie09@gmail.com>
2014-10-08 17:52:27 -0600

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject racehub/stripe-clj "0.1.0-SNAPSHOT"
+(defproject racehub/stripe-clj "0.1.0"
   :description "Schemafied Stripe bindings for Clojure."
   :url "https://github.com/racehub/stripe-clj"
   :license {:name "Eclipse Public License"
