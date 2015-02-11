757216d4024af7224f61aaf65c41a5ebe1f63777
Sam Ritchie <sritchie09@gmail.com>
2015-02-11 16:11:55 -0700

    lein-release plugin: bumped version from 0.1.6 to 0.1.7-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject racehub/stripe-clj "0.1.6"
+(defproject racehub/stripe-clj "0.1.7-SNAPSHOT"
   :description "Schemafied Stripe bindings for Clojure."
   :url "https://github.com/racehub/stripe-clj"
   :license {:name "Eclipse Public License"
